cc4a2b8cd502448b2b187bbdacfa81ec14dff19f
BALIREDDYLAKSHMANKUMAR <107310819+BALIREDDYLAKSHMANKUMAR@users.noreply.github.com>
2022-10-26 19:27:59 +0530

    Update App.java

--- genrics/App.java
+++ genrics/App.java
@@ -1,13 +1,19 @@
 package genrics;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello to Genrics Program!");
+         System.out.println("Give 3 inputs:");
+         Scanner sc = new Scanner(System.in);
+         IntegerMax i = new IntegerMax();
+         int inp1 = Integer.parseInt(sc.nextLine());
+         int inp2 = Integer.parseInt(sc.nextLine());
+         int inp3 = Integer.parseInt(sc.nextLine());
+         int ArrayOfInputs[] = { inp1, inp2, inp3 };
+         int max = i.CalculateMaximum(ArrayOfInputs);
+         i.checkMaxAt2ndPos(ArrayOfInputs[1], max);
     }
 }
